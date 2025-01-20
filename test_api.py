979f903f4a1194ebc71e3d965ef9d6857b663828
Evgenii K. <666metis@mail.ru>
2025-01-20 14:11:02 +0500

    Дипломная работа

--- test_api.py
+++ test_api.py
@@ -1,4 +1,6 @@
 import requests
+import allure
+import pytest
 
 
 baseURL = 'https://api.kinopoisk.dev/v1.4/'
@@ -7,22 +9,34 @@ headers = {
 'Content-Type': 'application/json'
 }
 
+
+@allure.suite("Заполнение и отправка формы")
+@allure.title("Тест отправки формы")
+@allure.description(
+    "Этот тест проряет заполняемость формы данными и проверяет заполненность каждого поля после отправки"
+)
+@allure.feature("Форма отправки")
 def test_series_id_information():
-    response = requests.get(f'{baseURL}movie/727157', headers = headers)
+    with allure.step("Отправить запрос на поиск сериала и получить ответ"):
+        response = requests.get(f'{baseURL}movie/727157', headers = headers)
     assert response.status_code == 200
 
 def test_film_id_information():
-    response = requests.get(f'{baseURL}movie/263531', headers=headers)
+    with allure.step("Отправить запрос на поиск фильма и получить ответ"):
+        response = requests.get(f'{baseURL}movie/263531', headers=headers)
     assert response.status_code == 200
 
 def test_director_id():
-    response = requests.get(f'{baseURL}movie/40182', headers=headers)
+    with allure.step("Отправить запрос на поиск режиссера и получить ответ"):
+        response = requests.get(f'{baseURL}movie/40182', headers=headers)
     assert response.status_code == 200
 
 def test_non_series():
-    response = requests.get(f'{baseURL}movie/000001', headers=headers)
+    with allure.step("Отправить невалидный запрос на поиск сериала и получить ответ"):
+        response = requests.get(f'{baseURL}movie/000001', headers=headers)
     assert response.status_code == 400
 
 def test_invalid_name():
-    response = requests.get(f'{baseURL}movie/Мст.ти!тели', headers=headers)
+    with allure.step("Отправить запрос с невалидным названием и получить ответ"):
+        response = requests.get(f'{baseURL}movie/Мст.ти!тели', headers=headers)
     assert response.status_code == 400
--- test_ui.py
+++ test_ui.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -10,31 +11,42 @@ def chrome_browser():
     yield driver
     driver.quit()
 
+@allure.suite("Заполнение и отправка формы")
+@allure.title("Тест отправки формы")
+@allure.description(
+    "Этот тест проряет заполняемость формы данными и проверяет заполненность каждого поля после отправки"
+)
+@allure.feature("Форма отправки")
 def test_search_name(chrome_browser):
-    chrome_browser.get("https://www.kinopoisk.ru/")
+    with allure.step("Переход на сайт, заполнение строки поиска и получение ответа на запрос"):
+        chrome_browser.get("https://www.kinopoisk.ru/")
     chrome_browser.find_element(By.NAME, "kp_query").send_keys("Мстители")
     chrome_browser.find_element(By.ID, "suggest-item-film-263531").click()
     assert chrome_browser.find_element(By.CSS_SELECTOR, "span[data-tid='75209b22']").text =="Мстители (2012)"
 
 def test_search_name_invalid(chrome_browser):
-    chrome_browser.get("https://www.kinopoisk.ru/")
+    with allure.step("Переход на сайт, заполнение строки поиска невалидными данными и получение ответа на запрос"):
+        chrome_browser.get("https://www.kinopoisk.ru/")
     chrome_browser.find_element(By.NAME, "kp_query").send_keys("Мст!тел№")
     assert chrome_browser.find_element(By.XPATH,"//*[contains(@class, 'emptySuggest')]").text == "По вашему запросу ничего не найдено"
 
 def test_search_english_name(chrome_browser):
-    chrome_browser.get("https://www.kinopoisk.ru/")
+    with allure.step("Переход на сайт, заполнение строки поиска на английском языке и получение ответа на запрос"):
+        chrome_browser.get("https://www.kinopoisk.ru/")
     chrome_browser.find_element(By.NAME, "kp_query").send_keys("Avengers")
     chrome_browser.find_element(By.ID, "suggest-item-film-843650").click()
     assert chrome_browser.find_element(By.CSS_SELECTOR, "span[data-tid='75209b22']").text =="Мстители: Финал (2019)"
 
 def test_by_valid_symbol(chrome_browser):
-    chrome_browser.get("https://www.kinopoisk.ru/")
+    with allure.step("Переход на сайт, заполнение строки поиска с помощью чисел и получение ответа на запрос"):
+        chrome_browser.get("https://www.kinopoisk.ru/")
     chrome_browser.find_element(By.NAME,"kp_query").send_keys("1+1")
     chrome_browser.find_element(By.ID,"suggest-item-film-535341").click()
     assert chrome_browser.find_element(By.CSS_SELECTOR,"span[data-tid='75209b22']").text =="1+1 (2011)"
 
 def test_search_by_actor(chrome_browser):
-    chrome_browser.get("https://www.kinopoisk.ru/")
+    with allure.step("Переход на сайт, заполнение строки поиска по имени актера и получение ответа на запрос"):
+        chrome_browser.get("https://www.kinopoisk.ru/")
     chrome_browser.find_element(By.NAME,"kp_query").send_keys("Вин Дизель")
     chrome_browser.find_element(By.ID,"suggest-item-person-11437").click()
     assert chrome_browser.find_element(By.CSS_SELECTOR,"h1[data-tid='f22e0093']").text =="Вин Дизель"
